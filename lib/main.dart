211bc3dcd43cf8e1b2618e432ac96a04ac7d3e96
ErickJPB <erick.tva1@gmail.com>
2021-02-26 21:03:50 -0300

    Versao-Final-Aula

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,24 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertube/blocs/favorite_bloc.dart';
+import 'package:fluttertube/blocs/videos_bloc.dart';
 import 'package:fluttertube/screens/home.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'FlutterTube',
-      home: Home(),
+    return BlocProvider(
+      bloc: VideosBloc(),
+      child: BlocProvider(
+        bloc: FavoriteBloc(),
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'FlutterTube',
+          home: Home(),
+        ),
+      ),
     );
   }
 }
-
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,9 +1,18 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertube/blocs/favorite_bloc.dart';
+import 'package:fluttertube/blocs/videos_bloc.dart';
 import 'package:fluttertube/delegates/data_search.dart';
+import 'package:fluttertube/models/video.dart';
+import 'package:fluttertube/screens/favorites.dart';
+import 'package:fluttertube/widgets/video_tile.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final bloc = BlocProvider.of<VideosBloc>(context);
+    final blocFav = BlocProvider.of<FavoriteBloc>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Container(
@@ -15,20 +24,63 @@ class Home extends StatelessWidget {
         actions: <Widget>[
           Align(
             alignment: Alignment.center,
-            child: Text(
-              "Erick",
-              style: TextStyle(fontSize: 20),
+            child: StreamBuilder<Map<String, Video>>(
+              stream: blocFav.outFav,
+              builder: (context, snapshot) {
+                if (snapshot.hasData)
+                  return Text("${snapshot.data.length}");
+                else
+                  return Container();
+              },
             ),
           ),
-          IconButton(icon: Icon(Icons.star), onPressed: () {}),
           IconButton(
-              icon: Icon(Icons.search),
-              onPressed: () {
-                showSearch(context: context, delegate: DataSearch());
-              }),
+            icon: Icon(Icons.star),
+            onPressed: () {
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => Favorites()));
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () async {
+              String result =
+                  await showSearch(context: context, delegate: DataSearch());
+              if (result != null) bloc.inSearch.add(result);
+            },
+          ),
         ],
       ),
-      body: Container(),
+      backgroundColor: Colors.black87,
+      body: StreamBuilder(
+        stream: bloc.outVideos,
+        initialData: [],
+        builder: (context, snapshot) {
+          if (snapshot.hasData)
+            return ListView.builder(
+              itemBuilder: (context, index) {
+                if (index < snapshot.data.length) {
+                  return VideoTile(snapshot.data[index]);
+                } else if (index > 1) {
+                  bloc.inSearch.add(null);
+                  return Container(
+                    height: 40,
+                    width: 40,
+                    alignment: Alignment.center,
+                    child: CircularProgressIndicator(
+                      valueColor: AlwaysStoppedAnimation<Color>(Colors.red),
+                    ),
+                  );
+                } else {
+                  return Container();
+                }
+              },
+              itemCount: snapshot.data.length + 1,
+            );
+          else
+            return Container();
+        },
+      ),
     );
   }
 }
